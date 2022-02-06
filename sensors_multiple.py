ad2b98c7bf18cdfa3c0236618656752001f0a92e
Matej Demsar <demsarmat@gmail.com>
2022-02-06 21:09:16 +0100

    Added temperature limits

--- sensors_multiple.py
+++ sensors_multiple.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 from difflib import diff_bytes
 from time import sleep
+from dotenv import load_dotenv
+from pathlib import Path
 from multiprocessing import Process
+import os
 import sys
 import Adafruit_DHT
 
@@ -17,7 +20,7 @@ def sens1():
         try:
             sens1_temp = sens1.temperature
 
-            print (
+            print(
                 "Temp: {0:0.1f}*C".format(
                     sens1_temp
                 )
@@ -39,7 +42,7 @@ def sens2():
         try:
             sens2_temp = sens2.temperature
 
-            print (
+            print(
                 "Temp: {0:0.1f}*C".format(
                     sens2_temp
                 )
@@ -61,7 +64,7 @@ def sens3():
         try:
             sens3_temp = sens3.temperature
 
-            print (
+            print(
                 "Temp: {0:0.1f}*C".format(
                     sens3_temp
                 )
@@ -83,7 +86,7 @@ def sens4():
         try:
             sens4_temp = sens4.temperature
 
-            print (
+            print(
                 "Temp: {0:0.1f}*C".format(
                     sens4_temp
                 )
@@ -105,7 +108,7 @@ def sens5():
         try:
             sens5_temp = sens5.temperature
 
-            print (
+            print(
                 "Temp: {0:0.1f}*C".format(
                     sens5_temp
                 )
@@ -127,7 +130,7 @@ def sens6():
         try:
             sens6_temp = sens6.temperature
 
-            print (
+            print(
                 "Temp: {0:0.1f}*C".format(
                     sens6_temp
                 )
@@ -143,6 +146,54 @@ def sens6():
             raise error
 
         sleep(2.0)
+
+def sens1warn():
+    if sens1.temperature >= int(os.getenv('WARNING_TEMP')):
+        print("Sensor 1 has reached over 60*C!")
+
+def sens2warn():
+    if sens2.temperature >= int(os.getenv('WARNING_TEMP')):
+        print("Sensor 2 has reached over 60*C!")
+
+def sens3warn():
+    if sens3.temperature >= int(os.getenv('WARNING_TEMP')):
+        print("Sensor 3 has reached over 60*C!")
+
+def sens4warn():
+    if sens4.temperature >= int(os.getenv('WARNING_TEMP')):
+        print("Sensor 4 has reached over 60*C!")
+
+def sens5warn():
+    if sens5.temperature >= int(os.getenv('WARNING_TEMP')):
+        print("Sensor 5 has reached over 60*C!")
+
+def sens6warn():
+    if sens6.temperature >= int(os.getenv('WARNING_TEMP')):
+        print("Sensor 6 has reached over 60*C!")
+
+def sens1crit():
+    if sens1.temperature >= int(os.getenv('CRIT_TEMP')):
+        print("Sensor 1 has reached over 70*C!")
+
+def sens2crit():
+    if sens2.temperature >= int(os.getenv('CRIT_TEMP')):
+        print("Sensor 2 has reached over 70*C!")
+
+def sens3crit():
+    if sens3.temperature >= int(os.getenv('CRIT_TEMP')):
+        print("Sensor 3 has reached over 70*C!")
+
+def sens4crit():
+    if sens4.temperature >= int(os.getenv('CRIT_TEMP')):
+        print("Sensor 4 has reached over 70*C!")
+
+def sens5crit():
+    if sens5.temperature >= int(os.getenv('CRIT_TEMP')):
+        print("Sensor 5 has reached over 70*C!")
+
+def sens6crit():
+    if sens6.temperature >= int(os.getenv('CRIT_TEMP')):
+        print("Sensor 6 has reached over 70*C!")
 
 if __name__ == '__main__':
 
@@ -163,3 +214,39 @@ if __name__ == '__main__':
 
     proc6 = Process(target=sens6)
     proc6.start()
+
+    proc7 = Process(target=sens1warn)
+    proc7.start()
+
+    proc8 = Process(target=sens2warn)
+    proc8.start()
+
+    proc9 = Process(target=sens3warn)
+    proc9.start()
+
+    proc10 = Process(target=sens4warn)
+    proc10.start()
+
+    proc11 = Process(target=sens5warn)
+    proc11.start()
+    
+    proc12 = Process(target=sens6warn)
+    proc12.start()
+
+    proc13 = Process(target=sens1crit)
+    proc13.start()
+
+    proc14 = Process(target=sens2crit)
+    proc14.start()
+
+    proc15 = Process(target=sens3crit)
+    proc15.start()
+
+    proc16 = Process(target=sens4crit)
+    proc16.start()
+
+    proc17 = Process(target=sens5crit)
+    proc17.start()
+    
+    proc18 = Process(target=sens6crit)
+    proc18.start()
